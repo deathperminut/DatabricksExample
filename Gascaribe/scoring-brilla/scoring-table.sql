55843bb539cbdc1d94c7ef75279b4b18c3a5c12b
AlbertAcevedo <AlbertAcevedo>
2023-02-08 14:44:54 +0000

    Scoring FNB - Unity catalog

---
+++ Gascaribe/scoring-brilla/scoring-table.sql
@@ -0,0 +1,38 @@
+-- Databricks notebook source
+CREATE TABLE IF NOT EXISTS analiticagdc.brilla.scoringFNB ( 
+    IdContrato      BIGINT,
+    CupoAsignado    BIGINT,
+    Identificacion  VARCHAR(20),
+    Tipo            VARCHAR(50),
+    Nodo            INTEGER,
+    Riesgo          VARCHAR(20),
+    Categoria       INTEGER,
+    Estrato         INTEGER,
+    FechaPrediccion DATE
+)
+
+-- COMMAND ----------
+
+CREATE CATALOG analiticagdc
+
+-- COMMAND ----------
+
+USE CATALOG analiticagdc;
+
+-- COMMAND ----------
+
+CREATE DATABASE brilla
+
+-- COMMAND ----------
+
+USE DATABASE brilla;
+CREATE TABLE scoringFNB CLONE delta.`abfss://gold@gdcbidatalake.dfs.core.windows.net/scoringFNB`
+
+-- COMMAND ----------
+
+SELECT *
+FROM analiticagdc.brilla.scoringFNB
+
+-- COMMAND ----------
+
+
