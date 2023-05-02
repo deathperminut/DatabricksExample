e6d7f4d64fc2be7ceb4435b403771fec5a7829b3
AlbertAcevedo <AlbertAcevedo>
2023-05-02 15:41:44 +0000

    Limpiar repo + cambiar variables de entorno del modelo de alivios

--- Gascaribe/days-outstanding-simulation/AverageDebt.sql
+++
@@ -1,8 +0,0 @@
-DECLARE @Fecha DATETIME = '2019-10-31';
-
-SELECT AVG( DeudaDiferida + DeudaCorrienteNoVencida + DeudaCorrienteVencida ) AS DeudaPromedio
-FROM Cartera.FactResumenCierreDia
-WHERE FechaCierre = @Fecha
-	AND EdadMora > 90
-	AND TipoProducto = 7055
-;
